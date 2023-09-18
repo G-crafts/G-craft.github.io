e22477fdf5fc660589144530d40a7def2d25aa1a
G-crafts <142952264+G-crafts@users.noreply.github.com>
2023-09-17 21:57:56 -0600

    Add files via upload

---
+++ G-crafts_EvenOddG-craftsWithAddedArrays_9-17-23.java
@@ -0,0 +1,27 @@
+// This is written by katelynn.graham@malad.us
+// for Mr. Gross Software dev11A
+// September 13-2023
+// Counts even or odd
+// 3b Flow Control Program
+// Added Array
+// September 15-2023
+
+
+class EvenOddArray {
+public static void main(String[] args) {
+    for (int i = 1; i <= 100; i++){
+        if(i%2 == 0){
+    System.out.println("The number "+i+" is an even number");
+   
+    }else {
+    System.out.println("The number "+i+" is an odd number");
+       for (int[] intArray = new int[]{1,10,20,30,40,50,60,70,80,90,100};
+            System.out.println("The number "+i+" is an even number");
+
+    }
+    }
+
+
+
+}
+}
