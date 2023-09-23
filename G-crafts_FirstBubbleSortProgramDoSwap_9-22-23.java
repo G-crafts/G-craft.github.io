59481376bdeaac6515334b6a13528a42b0ee5af8
G-crafts <142952264+G-crafts@users.noreply.github.com>
2023-09-22 18:26:49 -0600

    Add files via upload

---
+++ G-crafts_FirstBubbleSortProgramDoSwap_9-22-23.java
@@ -0,0 +1,21 @@
+// katelynn.graham@malad.us
+
+//for CTE software development 1
+
+//instructor MrGross
+
+
+class bubbleSort{
+    boolean doSwap = true
+    while (doSwap){
+        doSwap = false;
+        for (int i = 0;  array.length - 1; i++) {
+            if (array [i] > array[i + 1]) {
+                swap(array, i, i + 1);
+                swapTwoArrayElements = true;{
+                static int[] swapTwoArrayElements; int[]arrayToSwap;{
+            }
+        }
+    }
+    }
+}
