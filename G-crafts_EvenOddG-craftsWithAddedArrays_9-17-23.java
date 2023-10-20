f93174a9894ab377de60470c613e594f7114a481
G-crafts <142952264+G-crafts@users.noreply.github.com>
2023-10-19 23:07:59 -0600

    Update G-crafts_EvenOddG-craftsWithAddedArrays_9-17-23.java

--- G-crafts_EvenOddG-craftsWithAddedArrays_9-17-23.java
+++ G-crafts_EvenOddG-craftsWithAddedArrays_9-17-23.java
@@ -14,8 +14,11 @@ public static void main(String[] args) {
     System.out.println("The number "+i+" is an even number");
    
     }else {
+//Added Array Printed
     System.out.println("The number "+i+" is an odd number");
+// Integers 1-100
        for (int[] intArray = new int[]{1,10,20,30,40,50,60,70,80,90,100};
+//Integers array 1-100 Printed
             System.out.println("The number "+i+" is an even number");
 
     }
